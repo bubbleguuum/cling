030c361f03946dea3fa9b5ba1a25c8b19b15193b
Michael Pujos <bubbleguuum@free.fr>
2013-10-26 14:33:04 +0200

    set Content-Type header in outgoing event response

--- core/src/main/java/org/fourthline/cling/model/message/gena/OutgoingEventResponseMessage.java
+++ core/src/main/java/org/fourthline/cling/model/message/gena/OutgoingEventResponseMessage.java
@@ -19,6 +19,8 @@ package org.fourthline.cling.model.message.gena;
 
 import org.fourthline.cling.model.message.StreamResponseMessage;
 import org.fourthline.cling.model.message.UpnpResponse;
+import org.fourthline.cling.model.message.header.ContentTypeHeader;
+import org.fourthline.cling.model.message.header.UpnpHeader;
 
 /**
  * @author Christian Bauer
@@ -27,9 +29,11 @@ public class OutgoingEventResponseMessage extends StreamResponseMessage {
 
     public OutgoingEventResponseMessage() {
         super(new UpnpResponse(UpnpResponse.Status.OK));
+        getHeaders().add(UpnpHeader.Type.CONTENT_TYPE, new ContentTypeHeader());
     }
 
     public OutgoingEventResponseMessage(UpnpResponse operation) {
         super(operation);
+        getHeaders().add(UpnpHeader.Type.CONTENT_TYPE, new ContentTypeHeader());
     }
 }
